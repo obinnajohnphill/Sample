3272ea1f498a40ec63ffe738a1ce40ce61972c32
Obinna Johnphill <obinna.johnphill@enta.net>
2017-09-27 11:34:27 +0100

    first commit

---
+++ home.php
@@ -0,0 +1,13 @@
+<?php
+	/*** Important do not change ***/
+	session_start();
+	if(!isset($_SESSION['simple_login'])){
+		header("Location: index.php");
+		exit();
+	}
+	/*** Important END ***/
+?>
+
+<!-------- Write you stuff down here -------->
+<h1 align="center">Welcome, <?php echo $_SESSION['simple_login']; ?></h1>
+<p align="center"><a href="logout.php">Logout</a></p>
